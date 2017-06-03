430e34a436ca20299b71d8d613a23cdd83bda018
Michael Ficarra <git@michael.ficarra.me>
2017-06-03 15:34:14 -0700

    refactoring

--- src/index.js
+++ src/index.js
@@ -30,45 +30,59 @@ export syntax interface = ctx => {
     configurable: false, writable: false, enumerable: false
   },`;
 
+  function memberAccess(p) {
+    if (isIdentifier(p) || isKeyword(p)) {
+      return #`.`.concat(p);
+    } else if (isStringLiteral(p) || isNumericLiteral(p)) {
+      return #`[${p}]`;
+    } else if (isBrackets(p)) {
+      return p;
+    }
+    throw new Error('Unrecognised property name');
+  }
+
+  let fieldChecks = [], staticFieldChecks = [], methodDecls = [], staticMethodDecls = [];
+
+  items.forEach(i => {
+    switch (i.type) {
+      case 'field':
+        fieldChecks.push(#`if (klass.prototype[${name}.${i.name}] == null) throw new Error(${name}.${i.name}.toString() + ' not implemented by ' + klass);`);
+        break;
+      case 'static field':
+        staticFieldChecks.push(#`if (klass[${name}.${i.name}] == null) throw new Error(${name}.${i.name}.toString() + ' not implemented by ' + klass);`);
+        break;
+      case 'method': {
+        let p = i.name;
+        if (isIdentifier(p) || isKeyword(p)) {
+          p = #`.`.concat(p);
+        } else if (isStringLiteral(p) || isNumericLiteral(p)) {
+          p = #`[${p}]`;
+        }
+        methodDecls.push(#`klass.prototype ${p} = this ${p};`);
+        break;
+      }
+      case 'static method': {
+        let p = i.name;
+        if (isIdentifier(p) || isKeyword(p)) {
+          p = #`.`.concat(p);
+        } else if (isStringLiteral(p) || isNumericLiteral(p)) {
+          p = #`[${p}]`;
+        }
+        methodDecls.push(#`klass ${p} = this ${p};`);
+        break;
+      }
+    }
+  });
+
+  function join(ts) {
+    return ts.reduce((accum, t) => accum.concat(t), #``);
+  }
+
   let mixin = #`_mixin: { value: function (klass) {
-    ${items
-      .filter(i => i.type === 'field')
-      .reduce((acc, i) => acc.concat(#`if (klass.prototype[${name}.${i.name}] == null) throw new Error(${name}.${i.name}.toString() + ' not implemented');`), #``)
-    }
-    ${items
-      .filter(i => i.type === 'static field')
-      .reduce((acc, i) => acc.concat(#`if (klass[${name}.${i.name}] == null) throw new Error(${name}.${i.name}.toString() + ' not implemented');`), #``)
-    }
-    ${items
-      .filter(i => i.type === 'method')
-      .map(i => i.name)
-      .map(function memberAccess(p) {
-          if (isIdentifier(p) || isKeyword(p)) {
-            return #`.`.concat(p);
-          } else if (isStringLiteral(p) || isNumericLiteral(p)) {
-            return #`[${p}]`;
-          } else if (isBrackets(p)) {
-            return p;
-          }
-          throw new Error('Unrecognised property name');
-        })
-      .reduce((acc, i) => acc.concat(#`klass.prototype ${i} = this ${i};`), #``)
-    }
-    ${items
-      .filter(i => i.type === 'static method')
-      .map(i => i.name)
-      .map(function memberAccess(p) {
-          if (isIdentifier(p) || isKeyword(p)) {
-            return #`.`.concat(p);
-          } else if (isStringLiteral(p) || isNumericLiteral(p)) {
-            return #`[${p}]`;
-          } else if (isBrackets(p)) {
-            return p;
-          }
-          throw new Error('Unrecognised property name');
-        })
-      .reduce((acc, i) => acc.concat(#`klass ${i} = this ${i};`), #``)
-    }
+    ${join(fieldChecks)}
+    ${join(staticFieldChecks)}
+    ${join(methodDecls)}
+    ${join(staticMethodDecls)}
     this._superclasses.forEach(s => { s._mixin(klass); });
     return klass;
   }, configurable: false, writable: false, enumerable: false},`;
