38e3c481d53f466200440745c1f62d32dab186a4
Michael Ficarra <git@michael.ficarra.me>
2017-06-03 20:49:10 -0700

    tests for some expected failure cases

--- test/_compile.js
+++ test/_compile.js
@@ -1,21 +1,22 @@
-import { compile } from '@sweet-js/core';
+import { compile as sweet } from '@sweet-js/core';
 import NodeLoader from '@sweet-js/core/dist/node-loader';
 import { transform } from 'babel-core';
 import { writeFileSync, readFileSync } from 'fs';
 import { fileSync } from 'tmp';
 
 export function compileAndEval(code) {
+  return eval(compile(code));
+}
+
+export function compile(code) {
   code = `'lang sweet.js';
 import { class, interface, implements } from '../src/index';
-output = (function() { ${code} }());`;
+(function() { ${code} }());`;
 
   let output;
   let outFile = fileSync({ dir: __dirname });
   writeFileSync(outFile.fd, code);
 
   let loader = new NodeLoader(__dirname);
-  let res = transform(compile(outFile.name, loader).code, {"plugins": ["transform-es2015-modules-commonjs"]}).code;
-  // console.log(res);
-  eval(res);
-  return output;
+  return transform(sweet(outFile.name, loader).code, {"plugins": ["transform-es2015-modules-commonjs"]}).code;
 }
--- test/test-algebras.js
+++ test/test-algebras.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import { compileAndEval } from './_compile';
+import { compileAndEval, compile } from './_compile';
 
 test('an interface is an object', t => {
   t.deepEqual('object', compileAndEval(`
@@ -104,6 +104,23 @@ test('a class can implement multiple interfaces', t => {
 });
 
 
+test('an unimplemented interface throws', t => {
+  let code = compile(`
+    interface I { a; }
+    class C implements I {}
+  `);
+  t.throws(() => { eval(code); });
+});
+
+test('an unimplemented interface throws', t => {
+  let code = compile(`
+    interface I { static a; }
+    class C implements I {}
+  `);
+  t.throws(() => { eval(code); });
+});
+
+
 test('interfaces can extend other interfaces', t => {
   t.deepEqual({ c: 'function', d: 'function' }, compileAndEval(`
     interface I {
@@ -156,6 +173,75 @@ test('minimal implementations', t => {
   `));
 });
 
+// TODO: interleave static/proto methods
+test('evaluation order is preserved', t => {
+  t.deepEqual({
+    c0: 0,
+    c1: 1,
+    c3: 3,
+    c4: 4,
+    d0: 0,
+    d1: 1,
+    e3: 3,
+    e4: 4,
+    tc0: "function",
+    tc1: "function",
+    tc3: "function",
+    tc4: "function",
+    td0: "function",
+    td1: "function",
+    te3: "function",
+    te4: "function",
+  }, compileAndEval(`
+    let counter = 0;
+    interface I {
+      a;
+      [counter++]() { return 0; }
+      [counter++]() { return 1; }
+    }
+    interface K extends (counter++, I) {
+      b;
+      [counter++]() { return 3; }
+      [counter++]() { return 4; }
+    }
+    class C implements I, K {
+      [I.a]() {}
+      [K.b]() {}
+    }
+    class D implements I {
+      [I.a]() {}
+    }
+    class E implements K {
+      [I.a]() {}
+      [K.b]() {}
+    }
+    let c = new C;
+    let d = new D;
+    let e = new E;
+    return {
+      tc0: typeof c[0],
+      tc1: typeof c[1],
+      c0: c[0](),
+      c1: c[1](),
+      tc3: typeof c[3],
+      tc4: typeof c[4],
+      c3: c[3](),
+      c4: c[4](),
+
+      td0: typeof d[0],
+      td1: typeof d[1],
+      d0: d[0](),
+      d1: d[1](),
+
+      te3: typeof e[3],
+      te4: typeof e[4],
+      e3: e[3](),
+      e4: e[4](),
+    };
+  `));
+});
+
+
 
 test('implements operator', t => {
   t.deepEqual('success', compileAndEval(`
@@ -170,5 +256,30 @@ test('implements operator', t => {
     C implements I;
     let c = new C;
     return c.f();
+  `));
+});
+
+test('implements operator chaining', t => {
+  t.deepEqual({ a: 'function', b: 'function', f: 'function', g: 'function' }, compileAndEval(`
+    interface I {
+      a;
+      f() {}
+    }
+    interface K {
+      b;
+      g() {}
+    }
+    class C {
+      [I.a]() {}
+      [K.b]() {}
+    }
+    C implements I implements K;
+    let c = new C;
+    return {
+      a: typeof c[I.a],
+      b: typeof c[K.b],
+      f: typeof c.f,
+      g: typeof c.g,
+    };
   `));
 });
