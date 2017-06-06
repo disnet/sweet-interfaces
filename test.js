3384e9b71b872ef5e2896b685e7821d306ee0ac4
Michael Ficarra <git@michael.ficarra.me>
2017-06-06 13:10:11 -0700

    just some test garbage I had been keeping around forever

--- test.js
+++ test.js
@@ -1,11 +1,64 @@
 'lang sweet.js';
-import { class, interface } from './src/index';
+import { class, interface, implements } from './src/index';
+
+const _const = x => () => x;
+const _id = x => x;
 
 interface Functor {
   map;
+
+  void() {
+    return this[Functor.map](() => void 0);
+  }
+
+  voidRight(x) {
+    return this[Functor.map](() => x);
+  }
 }
 
-class Maybe implements Functor { }
+Function.prototype[Functor.map] = function(g) {
+  return x => this(g(x));
+};
+Function implements Functor;
+
+
+interface Apply extends Functor {
+  apply;
+
+  applyFirst(b) {
+    return _const[Functor.map](this)[Apply.apply](b);
+  }
+
+  applySecond(x) {
+    return _const(_id)[Functor.map](this)[Apply.apply](b);
+  }
+
+  lift2(f) {
+    return a => b => f[Functor.map](a)[Apply.apply](b);
+  }
+}
+
+Function.prototype[Apply.apply] = function (g) {
+  return x => this(x)(g(x));
+};
+Function implements Apply;
+
+
+interface Applicative extends Apply {
+  static pure;
+
+  when(b) {
+    return b ? _id : _const(this[Applicative.pure](void 0));
+  }
+}
+
+
+class Maybe implements Applicative {
+  static [Applicative.pure](x) {
+    return new Just(x);
+  }
+}
+
 class Just extends Maybe {
   constructor(value) {
     super();
@@ -15,9 +68,17 @@ class Just extends Maybe {
   [Functor.map](fn) {
     return new Just(fn(this.value));
   }
+
+  [Apply.apply](x) {
+    return x[Functor.map](this.value);
+  }
 }
 class Nothing extends Maybe {
   [Functor.map](fn) {
+    return this;
+  }
+
+  [Apply.apply](x) {
     return this;
   }
 }
