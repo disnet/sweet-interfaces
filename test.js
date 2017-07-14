db54839ea967509e14d450835a3f6dd069d70833
Tim Disney <tdisney@shapesecurity.com>
2017-07-13 17:54:16 -0700

    Change methods to symbols

--- test.js
+++ test.js
@@ -1,90 +1,26 @@
 'lang sweet.js';
 import { class, interface, implements } from './src/index';
 
-const _const = x => () => x;
-const _id = x => x;
-
-interface Functor {
-  map;
-
-  void() {
-    return this[Functor.map](() => void 0);
-  }
-
-  voidRight(x) {
-    return this[Functor.map](() => x);
-  }
+interface A {
+  a;
+  success() { return 'success'; }
 }
 
-Function.prototype[Functor.map] = function(g) {
-  return x => this(g(x));
-};
-Function implements Functor;
-
-
-interface Apply extends Functor {
-  apply;
-
-  applyFirst(b) {
-    return _const[Functor.map](this)[Apply.apply](b);
-  }
-
-  applySecond(x) {
-    return _const(_id)[Functor.map](this)[Apply.apply](b);
-  }
-
-  lift2(f) {
-    return a => b => f[Functor.map](a)[Apply.apply](b);
-  }
+interface B0 extends A {
+  b0;
 }
 
-Function.prototype[Apply.apply] = function (g) {
-  return x => this(x)(g(x));
-};
-Function implements Apply;
-
-
-interface Applicative extends Apply {
-  static pure;
-
-  when(b) {
-    return b ? _id : _const(this[Applicative.pure](void 0));
-  }
+interface B1 extends A {
+  b1;
+  [A.a]() {}
 }
 
-
-class Maybe implements Applicative {
-  static [Applicative.pure](x) {
-    return new Just(x);
-  }
+interface C extends B0, B1 {
+  c;
 }
 
-class Just extends Maybe {
-  constructor(value) {
-    super();
-    this.value = value;
-  }
-
-  [Functor.map](fn) {
-    return new Just(fn(this.value));
-  }
-
-  [Apply.apply](x) {
-    return x[Functor.map](this.value);
-  }
+class X implements C {
+  [B0.b0]() {}
+  [B1.b1]() {}
+  [C.c]() {}
 }
-class Nothing extends Maybe {
-  [Functor.map](fn) {
-    return this;
-  }
-
-  [Apply.apply](x) {
-    return this;
-  }
-}
-
-let j = new Just(1);
-
-console.log(Object.getOwnPropertySymbols());
-
-new Just(1)[Functor.map](console.log);
